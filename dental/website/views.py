77dcecddde8b29fc697e34fe3341fc28e119e990
Kurantowicz <u140631@wago.local>
2020-02-19 16:01:37 +0100

    5# POST method in the e-mail form

--- dental/website/views.py
+++ dental/website/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+    
+def contact(request):
+if request.method == "POST":
+    message_name = request.POST['message-name']
+    message_email = request.POST['message-email']
+    message = request.POST['message']
+    return render(request, 'contact.html', {})
+
+else:
+    return render(request, 'contact.html', {})
 
